fee7e278c4960361bd009b5d0da1b57a166ea74a
vuducnguyentb <vuducnguyentb@gmail.com>
2022-05-30 22:52:17 +0700

    create project laravel using sanctum

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    //
+    public function createProject(Request $request)
+    {
+
+    }
+
+    public function listProject()
+    {
+
+    }
+
+    public function singleProject($id){
+
+    }
+
+    public function deleteProject($id){
+
+    }
+}
